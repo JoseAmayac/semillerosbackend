1f92e895fe6416e0a7be3b47603126818a16f67d
José Amaya <jlacarrascal2684@gmail.com>
2020-09-28 20:43:29 -0500

    Iniciando auth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'prefix' => 'auth',
+    'middleware' => 'api'
+],function(){
+    Route::post('login','Api\AuthController@login');
+    Route::post('signup','Api\AuthController@signup');
 });
+    
