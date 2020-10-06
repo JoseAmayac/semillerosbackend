b69f7b4b1bdd29457c81173eda907ffc55b9dd44
Daniel Garcia <daniel.garciaa@autonoma.edu.co>
2020-10-05 20:29:18 -0500

    Cambios Swagger

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,9 +12,53 @@ use Illuminate\Support\Facades\Http;
 use GuzzleHttp\Client;
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     ** path="/v1/user-login",
+     *   tags={"Login"},
+     *   summary="Login",
+     *   operationId="login",
+     *
+     *   @OA\Parameter(
+     *      name="email",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *          type="string"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Correo electrónico o contraseña incorrectos"
+     *   )
+     *)
+     **/
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function login(Request $request){
+        $validator = User::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
         $credentials = request(['email', 'password']);
-
         if (!Auth::attempt($credentials)){
             return response()->json([
                 'message' => 'Correo electrónico o contraseña incorrectos'
@@ -24,7 +68,97 @@ class AuthController extends Controller
         return $this->getTokenAndRefreshToken($oClient,request('email'), request('password'));
     }
 
+
+    /**
+     * @OA\Post(
+     ** path="/v1/user-register",
+     *   tags={"Sign Up"},
+     *   summary="Sign Up",
+     *   operationId="sign up",
+     *
+     *  @OA\Parameter(
+     *      name="name",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *  @OA\Parameter(
+     *      name="lastname",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *  @OA\Parameter(
+     *      name="email",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *       name="cellphone",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="integer"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *      @OA\Parameter(
+     *      name="password_confirmation",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *     @OA\Parameter(
+     *      name="department_id",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="integer"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=201,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Invalid Data"
+     *   )
+     *)
+     **/
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function signup(SignUpRequest $request){
+        $validator = User::make($request->all(), [
+            'name' => 'required',
+            'lastname'=>'required',
+            'email' => 'required|email|unique:users',
+            'cellphone' => 'required|unique',
+            'password' => 'required|confirmed',
+            'department_id' => 'required' 
+        ]);
         $data = $request->all();
         $data['password'] = bcrypt($request->password);
         $user = User::create($data);
@@ -34,15 +168,48 @@ class AuthController extends Controller
         return $this->login($request);
     }
 
+
+    /**
+     * @OA\Post(
+     ** path="/v1/user-logout",
+     *   tags={"Login"},
+     *   summary="Login",
+     *   operationId="login",
+     *
+     *   @OA\Parameter(
+     *      name="token",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=203,
+     *       description="Cierre de sesion correcto",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *  @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *  )
+     *)
+     **/
+    /**
+     * logout api
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
-
         return response()->json([
             'message' => 'Cierre se sesión correcto'
         ],203);
     }
-
+    
     public function getTokenAndRefreshToken($oClient,$email,$password){
         $http = new Client;
 
@@ -65,6 +232,30 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * @OA\GET(
+     ** path="/v1/me",
+     *   tags={"Login"},
+     *   summary="Login",
+     *   operationId="login",
+     *  @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *  @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *   )
+     * )
+     */
+    /**
+     * me api
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function me(){
         return response()->json([
             'user' => request('user')
--- app/Http/Controllers/Api/DepartmentController.php
+++ app/Http/Controllers/Api/DepartmentController.php
@@ -14,6 +14,24 @@ class DepartmentController extends Controller
         $this->middleware("auth:api")->only("store", "update", "destroy");
     }
     /**
+     * @OA\Get(
+     *      path="/api/departments",
+     *      operationId="getDepartmentList",
+     *      tags={"Departments"},
+     *      security={
+     *      {"passport": {}},
+     *      summary="Get list of departments",
+     *      description="Returns list of departments",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *           mediaType="application/json",
+     *          )
+     *      )
+     *  )
+     */
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -24,9 +42,45 @@ class DepartmentController extends Controller
 
         return response()->json([
             'departments' => $departments
-        ]);
+        ], 201);
     }
 
+    /**
+     * @OA\Post(
+     ** path="/",
+     *   tags={"Department"},
+     *   summary="Store new department",
+     *   operationId="storeDepartment",
+     *
+     *  @OA\Parameter(
+     *      name="name",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *  @OA\Parameter(
+     *      name="description",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=201,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Invalid Data"
+     *   )
+     *)
+     **/
     /**
      * Store a newly created resource in storage.
      *
@@ -36,13 +90,54 @@ class DepartmentController extends Controller
     public function store(DepartmentRequest $request)
     {
         $department = Department::create($request->all());
-
         return response()->json([
             'message' => 'Departamento creado correctamente',
             'department' => $department
         ],201);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/departments/{id}",
+     *      operationId="getDepartmentById",
+     *      tags={"Departments"},
+     *      summary="Get department information",
+     *      description="Returns department data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Department id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="name",
+     *          in="query",
+     *          required=true,
+     *       @OA\Schema(
+     *            type="string"
+     *        )
+     *       ),
+     *       @OA\Parameter(
+     *         name="description",
+     *         in="query",
+     *         required=true,
+    *         @OA\Schema(
+    *               type="string"
+    *          )
+    *         ),
+     *      @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         response=201,
+     *         description="Success",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *         )
+     * )
+     */
     /**
      * Display the specified resource.
      *
@@ -56,6 +151,37 @@ class DepartmentController extends Controller
         ],200);
     }
 
+    /**
+     * @OA\Put(
+     *      path="/departments/{id}",
+     *      operationId="updateDepartment",
+     *      tags={"Departments"},
+     *      summary="Update existing department",
+     *      description="Returns updated department data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Department id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/UpdateProjectRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=202,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Project")
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      )
+     * )
+     */
     /**
      * Update the specified resource in storage.
      *
