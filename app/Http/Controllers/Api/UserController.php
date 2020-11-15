63b8ee5dbc7f21228006b70ea9c13b0908217d43
Daniel Garcia <daniel.garciaa@autonoma.edu.co>
2020-11-15 15:21:24 -0500

    Cambios Domingo

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('checkauth');
+        $this->middleware('checkauth')->except('show');
     }
 
     /**
