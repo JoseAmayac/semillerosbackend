732abdd015563fa42bb919794a2912f5a7a06ef0
diligente <diligenteexpress@gmail.com>
2020-11-27 01:40:51 -0500

    Aplicando middleware

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -212,7 +212,7 @@ class AuthController extends Controller
     
     public function getTokenAndRefreshToken($oClient,$email,$password){
         $http = new Client;
-        $response = $http->request('POST', config('app.url').':3190/oauth/token', [
+        $response = $http->request('POST', config('app.url').'/oauth/token', [
             'form_params' => [
                 'grant_type' => 'password',
                 'client_id' => $oClient->id,
@@ -314,7 +314,7 @@ class AuthController extends Controller
 
         $http = new Client;
         $oClient = OClient::where('password_client', 1)->first();
-        $response = $http->post(config('app.url').':3190/oauth/token', [
+        $response = $http->post(config('app.url').'/oauth/token', [
             'form_params' => [
                 'grant_type' => 'refresh_token',
                 'refresh_token' => $request->refresh_token,
